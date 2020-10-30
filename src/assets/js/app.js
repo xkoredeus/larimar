4615e56b3684cb4e14ed3e838271e001a9156ae6
Начаров Сергей <nacharovsa@it2g.ru>
2020-10-30 18:01:43 +0400

    feat: gallery

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -20,9 +20,10 @@ $(() => {
 });
 
 $(() => {
-    const mainSlider = new Swiper('.gallery-slider', {
+    const gallerySlider = new Swiper('.gallery-slider', {
         spaceBetween: 20,
         slidesPerView: 2,
+        loop: true,
         breakpoints: {
             0: {
                 slidesPerView: 1,
@@ -37,6 +38,7 @@ $(() => {
 $(() => {
     $('[data-fancybox]').fancybox({
         animationDuration: 600,
+        animationEffect   : 'slide-in-in',
         touch: false
     });
 });
@@ -77,4 +79,56 @@ $(() => {
             swiper: cardPreviewSlider
         }
     });
-})
+});
+
+$(() => {
+    $('.quantity').on('click', '.quantity-minus, .quantity-plus', function quantityClick() {
+        const input = $( this ).siblings( '.quantity-num' );
+        if ( (input.val() > 1) && ($( this ).hasClass( 'quantity-minus' ) ) ) {
+            input.val( +input.val() - 1 );
+        } else if (  (input.val() < +input.attr('max') ) && ( $( this ).hasClass( 'quantity-plus' ) ) ) {
+            input.val( +input.val() + 1 );
+        };
+    });
+});
+
+$(() => {
+    $('.tabs__content-item:not(:first-child)').hide();
+    $('.tabs__content-item:first-child').addClass('active');
+    $('.tabs__list-item:first-child').addClass('active');
+    $('.tabs__list > .tabs__list-item').click(function tabListLiClick() {
+        if (!($(this).hasClass('active'))) {
+            const thisLi = $(this);
+            const numLi = thisLi.index();
+            thisLi.addClass('active').siblings().removeClass('active');
+            thisLi.parent().next().children('div').hide().removeClass('active')
+                .eq(numLi)
+                .addClass('active')
+                .fadeIn('slow');
+        }
+    });
+});
+
+$(() => {
+    const matchSlider = new Swiper('.match', {
+        spaceBetween: 20,
+        navigation: {
+            nextEl: '.match__nav .swiper-button-next',
+            prevEl: '.match__nav .swiper-button-prev',
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+            },
+            500: {
+                slidesPerView: 2
+            },
+            800: {
+                slidesPerView: 3
+            },
+            1200: {
+                slidesPerView: 4
+            },
+        }
+    });
+});
